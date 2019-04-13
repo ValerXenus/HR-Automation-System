b559c833b37f1f8e7edf9bca674984e6e5ed3ba0
Valery <valeraxenus@mail.ru>
2019-04-13 10:29:28 +0300

    Adding project

---
+++ HR Automation System/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace HR_Automation_System
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
