6c76f3915b3890e933944b50600d428903d5c5eb
Valery <valeraxenus@mail.ru>
2019-04-14 22:01:29 +0300

    Доработал запрос

--- HR Automation System/Classes/DatabaseUtility.cs
+++ HR Automation System/Classes/DatabaseUtility.cs
@@ -34,12 +34,11 @@ namespace HR_Automation_System.Classes
         public void AddFamilyStatus(string status)
         {
             OleDbCommand cmd = new OleDbCommand();
-            cmd.Connection = _connection;
+            cmd.Connection = _connection;            
 
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = "INSERT INTO family_statuses VALUES ([Status_Index], [Status_Name])";
-            //cmd.CommandText = "INSERT INTO family_statuses VALUES ([Status_Name])";
-            cmd.Parameters.AddWithValue("@Status_Index", 1);
+            cmd.Parameters.AddWithValue("@Status_Index", 0);
             cmd.Parameters.AddWithValue("@Status_Name", status);
             try
             {
@@ -50,15 +49,15 @@ namespace HR_Automation_System.Classes
                 MessageBox.Show("Не удалось создать запрос\n" + ex.Message);
             }
 
-            cmd.CommandText = "SELECT LAST(status_id) FROM family_statuses";            
+            cmd.CommandText = "SELECT LAST(status_id) AS return_value FROM family_statuses";
             OleDbDataReader dr = cmd.ExecuteReader();
 
             int id = 0;
 
             while (dr.Read())
             {
-                id = int.Parse(dr["Expr1000"].ToString());
-            }            
+                id = int.Parse(dr["return_value"].ToString());
+            }
         }
     }
 }
