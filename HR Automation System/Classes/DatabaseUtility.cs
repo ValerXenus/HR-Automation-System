106ef93d5a87ba73a80c727a4c145053989455f7
Valery <valeraxenus@mail.ru>
2019-04-18 18:43:39 +0300

    Добавил индексацию записей

--- HR Automation System/Classes/DatabaseUtility.cs
+++ HR Automation System/Classes/DatabaseUtility.cs
@@ -9,55 +9,71 @@ namespace HR_Automation_System.Classes
     public class DatabaseUtility
     {
         private OleDbConnection _connection;
+        private OleDbCommand _command;
 
         public DatabaseUtility()
         {
-            Settings settings = new Settings();
+            Settings settings = new Settings(); // Создаем экземпляр настроек
             _connection = new OleDbConnection();
-            _connection.ConnectionString = settings.databaseConnectionString;
+            _connection.ConnectionString = settings.databaseConnectionString; // Получаем строку подключения
 
             try
             {
-                _connection.Open();
+                _connection.Open(); // Подключение к БД
             }
             catch
             {
                 MessageBox.Show("Ошибка подключения к базе данных");
             }
+
+            _command = new OleDbCommand(); // Инициализация экземпляра для команд
+            _command.Connection = _connection;
         }
 
         public void Disconnect()
         {
-            _connection.Close();
+            _connection.Close(); // Завершение подключения с БД
         }
 
         public void AddFamilyStatus(string status)
         {
-            OleDbCommand cmd = new OleDbCommand();
-            cmd.Connection = _connection;            
+            int idx = getLastIndex("family_statuses", "status_id"); // Получаем индекс последней записи в таблице и прибавляем 1
 
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "INSERT INTO family_statuses VALUES ([Status_Index], [Status_Name])";
-            cmd.Parameters.AddWithValue("@Status_Index", 0);
-            cmd.Parameters.AddWithValue("@Status_Name", status);
+            _command.CommandType = CommandType.Text;
+            _command.CommandText = "INSERT INTO family_statuses VALUES ([Status_Index], [Status_Name])";
+            _command.Parameters.AddWithValue("@Status_Index", idx);
+            _command.Parameters.AddWithValue("@Status_Name", status);
             try
             {
-                cmd.ExecuteNonQuery();
+                _command.ExecuteNonQuery();
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Не удалось создать запрос\n" + ex.Message);
-            }
+                MessageBox.Show("Не удалось выполнить запрос\n" + ex.Message);
+            }            
+        }
 
-            cmd.CommandText = "SELECT LAST(status_id) AS return_value FROM family_statuses";
-            OleDbDataReader dr = cmd.ExecuteReader();
+        // Получить индекс последней добавленной записи в таблице
+        private int getLastIndex(string tableName, string fieldName)
+        {
+            _command.CommandText = string.Format("SELECT LAST({0}) AS return_value FROM {1}", fieldName, tableName);
+            OleDbDataReader dr = _command.ExecuteReader();
 
-            int id = 0;
+            int idx = 0;
 
-            while (dr.Read())
+            try
             {
-                id = int.Parse(dr["return_value"].ToString());
+                while (dr.Read())
+                {
+                    idx = int.Parse(dr["return_value"].ToString());
+                }
             }
+            catch
+            {
+                // Если записей нет в таблице, то вернется 0
+            }
+
+            return idx;
         }
     }
 }
