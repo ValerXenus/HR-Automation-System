56df0a166d7232409863986f951de7cae02091c4
Valery <valeraxenus@mail.ru>
2019-04-20 09:05:47 +0300

    Добавил окно авторизации

---
+++ HR Automation System/Pages/LogInPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HR_Automation_System.Pages
+{
+    /// <summary>
+    /// Interaction logic for LogInPage.xaml
+    /// </summary>
+    public partial class LogInPage : Window
+    {
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
