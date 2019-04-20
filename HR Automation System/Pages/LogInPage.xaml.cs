d03737dfb39d61d01574fbdf67676c4311e68e28
Valery <valeraxenus@mail.ru>
2019-04-20 13:13:31 +0300

    Добавил Watermark для полей

--- HR Automation System/Pages/LogInPage.xaml.cs
+++ HR Automation System/Pages/LogInPage.xaml.cs
@@ -11,5 +11,31 @@ namespace HR_Automation_System.Pages
         {
             InitializeComponent();
         }
+
+        // Добавляем/убираем водяной текст для поля "Логин"
+        private void LoginTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (LoginTextBox.Text.Length == 0)
+            {
+                LoginWatermark.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                LoginWatermark.Visibility = Visibility.Hidden;
+            }
+        }
+
+        // Добавляем/убираем водяной текст для поля "Пароль"
+        private void PasswordTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (PasswordTextBox.Password.Length == 0)
+            {
+                PasswordWatermark.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PasswordWatermark.Visibility = Visibility.Hidden;
+            }
+        }        
     }
 }
